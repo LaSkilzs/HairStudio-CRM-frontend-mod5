c3d08c81c9f4f33e7a05c1d543f42703dc1305f3
La Fountain <github email address>
2019-02-24 17:54:30 -0500

    Complete routing for all pages

--- src/app/Login.js
+++ src/app/Login.js
@@ -4,7 +4,9 @@ import Profile from "../profile/Profile";
 import Appointment from "../appointment/Appointment";
 import NewsFeed from "../newsfeed/NewsFeed";
 import Calendar from "../calendar/Calendar";
-import LogNav from "../app/LogNav";
+import Dashboard from "../dashboard/Dashboard";
+import GalleryContainer from "../gallery/GalleryContainer";
+import { Switch, Route } from "react-router-dom";
 import "../css/login.css";
 
 class Login extends React.Component {
@@ -12,12 +14,15 @@ class Login extends React.Component {
     return (
       <section id="Login">
         <div className="login-container">
-          <LoginCard />
-          <LogNav />
-          <Profile />
-          <Appointment />
-          <Calendar />
-          <NewsFeed />
+          <Switch>
+            <Route path="/login" component={LoginCard} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/calendar" component={Calendar} />
+            <Route path="/newsfeed" component={NewsFeed} />
+            <Route path="/gallery" component={GalleryContainer} />
+            <Route path="/appointment" component={Appointment} />
+          </Switch>
         </div>
       </section>
     );
