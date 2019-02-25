0f61815b254a97adbb2e46fc8e6e8cb6dec2ac45
La Fountain <github email address>
2019-02-24 19:45:44 -0500

    Calender imported and functional

--- src/appointment/AppointmentCard.js
+++ src/appointment/AppointmentCard.js
@@ -1,11 +1,41 @@
 import React from "react";
+import DayPicker from "react-day-picker";
+import "react-day-picker/lib/style.css";
 
-const AppointmentCard = () => {
-  return (
-    <div className="appointment-card">
-      <h3>Appointment Card is here!</h3>
-    </div>
-  );
-};
+class AppointmentCard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedDate: undefined
+    };
+  }
 
+  handleDayClick = (day, { selected, disabled }) => {
+    if (disabled) {
+      return;
+    }
+    if (selected) {
+      this.setState({ selectedDay: undefined });
+      return;
+    }
+    this.setState({ selectedDay: day });
+  };
+
+  render() {
+    return (
+      <div className="appointment-card">
+        <DayPicker
+          onDayClick={this.handleDayClick}
+          selectedDays={this.state.selectedDay}
+          disabledDays={{ daysOfWeek: [0, 1] }}
+        />
+        {this.state.selectedDay ? (
+          <p>You clicked {this.state.selectedDay.toLocaleDateString()}</p>
+        ) : (
+          <p> Please select a day.</p>
+        )}
+      </div>
+    );
+  }
+}
 export default AppointmentCard;
